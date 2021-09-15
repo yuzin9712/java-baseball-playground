b9b37fd825c061e54d0bf4bdf24dece4ce3b2d40
yuzin9712 <dbwls97@ajou.ac.kr>
2021-09-15 22:14:28 +0900

    학습테스트 실습 완료

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,42 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        int size = numbers.size();
+        Assertions.assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        Assertions.assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void isContain(Integer input, Boolean expected) {
+        Assertions.assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,44 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String actual = "1,2";
+        String[] result = actual.split(",");
+        assertThat(result).contains("1", "2");
+
+        actual = "1";
+        result = actual.split(",");
+        assertThat(result).containsExactly("1");
+
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)";
+
+        String result = actual.substring(1, actual.length() - 1);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("StringIndexOutOfBounds Exception")
+    @Test
+    void charAt() {
+        String actual = "abc";
+        assertThatThrownBy(() -> {
+            actual.charAt(4);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
